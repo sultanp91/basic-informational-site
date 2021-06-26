f97fb02eac0f8fa97a6d9545560fc537fe6dd7ca
Sultan <sultanp@gmail.com>
2021-06-26 13:45:18 +0100

    created http server

---
+++ index.js
@@ -0,0 +1,26 @@
+const url = require('url');
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+    const q = url.parse(req.url, true);
+    if(q.pathname === "/"){
+        fs.readFile('index.html', function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+          });
+    } else {const filename = "." + q.pathname + ".html";
+    fs.readFile(filename, function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      } 
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+
+    }
+    
+  }).listen(8080);
