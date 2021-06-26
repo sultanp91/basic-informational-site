4d64fe80f71ef3dbef9be3f9e6e97e63b1c6f1a5
Sultan <sultanp@gmail.com>
2021-06-26 21:09:36 +0100

    updated routes on server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 http.createServer(function (req, res) {
     const q = url.parse(req.url, true);
@@ -13,12 +14,16 @@ http.createServer(function (req, res) {
     } else {const filename = "." + q.pathname + ".html";
     fs.readFile(filename, function(err, data) {
       if (err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end("404 Not Found");
-      } 
-      res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.readFile(path.join("404.html"), function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(data)
+            return res.end()
+        })
+      } else {res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
       return res.end();
+      }
+      
     });
 
     }
